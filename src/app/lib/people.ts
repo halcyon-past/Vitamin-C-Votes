c02e0252dd757d288f7ee22a1ab3a449ac038f09
halcyon-past <titanssuperior@gmail.com>
2024-07-09 12:18:49 +0530

    updated girls

--- src/app/lib/people.ts
+++ src/app/lib/people.ts
@@ -1,5 +1,5 @@
 const guys = ['Aritro Saha', 'Debrati Ta', 'Aratrik', 'Sujay Ghosh', 'Jaish Das', 'Ayishik', 'Aneek Saha','Biswajyoti Dutta','Ashrit Saha','Rayan','Pratyush','Soham Pal','Samiddho Chatterjee','Aritra Ganai','Aritra Sengupta','Aritra Bhadra','Subhadeep Paul','Sayan Deb','Sattwik','Dilnawaz','Parthiba','Mainak','Hridik Dasgupta','Sagar Mondal','Priyam Chowdhury','Pabak Kundu','Siddhabrata','Aniket Chattopadhya','Ayanjit Chakraborty','Sayan Nath','Sayak','Souhardya','Debdwaipayan Biswas','Spandan Mukherjee','Spandan Bhowmick','Ankan Routh','Barneel','Saptarshi Mazumder','Animesh','Arkoporno','Praneet','Bitan','Anwita','Gaurav','Mayukh','Sandhit'];
-const girls = ['Rupkatha De', 'Poulami Karmakar', 'Madhulika Majumder', 'Sharanya Giri','Nayoneeka','Dhriti','Diksha Banik','Nandini','Sreyashi','Aishani','Diya Das','Rishima Chowdhury','Priya Jha','Meghna','Meghavika','Dehali'];
+const girls = ['Rupkatha De', 'Poulami Karmakar', 'Madhulika Majumder', 'Sharanya Giri','Nayoneeka','Dhriti','Diksha Banik','Nandini','Sreyashi','Aishani','Diya Das','Rishima Chowdhury','Priya Jha','Meghna','Meghavika','Dehali','Anusha Bera','Anushmita Panji'];
 const combined = guys.concat(girls);
 
 guys.sort();
